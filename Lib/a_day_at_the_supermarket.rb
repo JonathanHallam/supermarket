30c39d74eb620c8e1b85f127e3b1ea918edf5db1
Jonathan Hallam <jonathanhallam@gds5065.local>
2017-01-27 14:05:16 +0000

    The staff discount option has now been added if using an appropriate staff number

--- Lib/a_day_at_the_supermarket.rb
+++ Lib/a_day_at_the_supermarket.rb
@@ -1,21 +1,19 @@
-require './basket.rb'
+require './Lib/basket.rb'
 
 puts "Hello, do you have a staff number?"
 puts "Y N"
 print ">"
 sn_response = $stdin.gets.chomp.upcase
 if sn_response == "Y"
-  puts "Enter staff number now"
+  puts "Enter staff number"
   print ">"
   staff_number = $stdin.gets.chomp
+  x = start_employee
+  name = x.check_name(staff_number)
+  puts x.greeting(name)
 
 # Check staff number
 
-# If manager x = start_manager
-# elsif != manager && employment < 6
-  # x = start_general
-# elsif emplyment >= 6
-  # x = start_employee
 
 elsif sn_response == "N"
   puts "Do you have a loyalty card number?"
@@ -35,7 +33,7 @@ elsif sn_response == "N"
   end
 end
 
-puts greeting
+
 puts "What would you like to do?"
 puts "add, remove, running total or checkout?"
 print ">"
